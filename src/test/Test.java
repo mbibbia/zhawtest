58253f43a4d1d4247f6f612e2873ee6f8964b014
christian <christian@orion>
2018-12-07 20:00:50 +0100

    Zweites Commit

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("geschafft!");
 		System.out.println("erste Änderung!");
+		System.out.println("zweite Änderung!");
 	}
 
 }
